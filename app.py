6fdfdaa0a089f92ca42d96cf8dd297ff90bc0b89
J4ngel <andres_angel@live.com.ar>
2021-11-11 06:51:50 -0500

    Manejo de bootstrap con y sin flask ojo se necesita instalar flask-bootstrap en el entorno virtual (pip install flask-bootstrap

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
-@app.route("/")
-def inicio():
-    return render_template("principal.html")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
